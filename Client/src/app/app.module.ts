9279a1d7d3d264484e7415df301607eefd489b15
İlker Bugüner <ilker.buguner@gmail.com>
2020-11-04 17:20:56 +0200

    Created Register and Login functionality with validation and added basic routing.

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
+import { AuthService } from './services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Client/src/app/register/register.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.registerForm = this.fb.group({
+      'username': ['', [Validators.required]],
+      'fullName': ['', [Validators.required]],
+      'email': ['', [Validators.required]],
+      'password': ['', [Validators.required]]
+    })
+   }
+
+  ngOnInit(): void {
+  }
+
+  register() {
+    this.authService.register(this.registerForm.value).subscribe(data => {
+      this.authService.saveToken(data['token']);
+    })
+  }
+
+  get username() {
+    return this.registerForm.get('username');
+  }
+
+  get fullName() {
+    return this.registerForm.get('fullName');
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+}
